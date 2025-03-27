5891c2dbaf78c14c45b4e7639ff944202833db24
Artem Borysenko <artem_borysenko@epam.com>
2025-03-27 17:16:57 +0100

    Interest Calculator App

--- interestCalculator/src/App.jsx
+++ interestCalculator/src/App.jsx
@@ -1,7 +1,34 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import {useState} from "react";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 6
+    });
+
+    const isInputValid = userInput.duration >= 1
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevState => {
+            return {
+                ...prevState,
+                [inputIdentifier]: +newValue
+            }
+        })
+    }
+
+    return (<>
+            <Header/>
+            <UserInput userInput={userInput} handleChange={handleChange}/>
+            {!isInputValid && <p className="center"> Please input duration greater than zero.</p>}
+            {isInputValid && <Results userInput={userInput}/>}
+        </>
+    )
 }
 
 export default App
